1fb68cc89ce2f62efb36e2fbf6258351e5c5377e
Gyurkó Levente Péter <gleventepter@mitiszkad.lan>
2023-11-13 09:25:53 +0100

    Kesz

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [get, set] = useState([]);
+  useEffect(() => {
+    fetch("https://pizza.kando-dev.eu/Pizza")
+      .then((response) => response.json())
+      .then((data) => set(data));
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {get.map((pizza) => (
+        <div>
+          <h1>{pizza.id}</h1>
+          <h1>{pizza.name}</h1>
+          <h1>{pizza.isGlutenFree}</h1>
+          <img width="200px" height="400px" src={pizza.kepURL}></img>
+        </div>
+      ))}
     </div>
   );
 }
